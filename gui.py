1ffea84c5ed00373f7b5dcbbd615d1572bc88223
dhagrow <cymrow@gmail.com>
2018-10-25 10:12:56 -0400

    more gui

--- gui.py
+++ gui.py
@@ -1,43 +1,70 @@
+import threading
+
 import sage
+
 from PySide2.QtCore import Qt
 from PySide2 import QtCore, QtGui, QtWidgets
 
 URL = 'tcp://localhost:6336'
 
-def execute():
-    print('boop')
-    c = sage.Client(URL)
+class Window(QtWidgets.QWidget):
+    def __init__(self, url):
+        super().__init__()
 
-    c.evaluate('print(1)')
+        self._client = sage.Client(url)
+        self.setup()
+
+        start_thread(self._output)
+
+    def setup(self):
+        self.action_quit = QtWidgets.QAction()
+        self.action_quit.setShortcut(QtGui.QKeySequence.Quit)
+        self.action_quit.triggered.connect(self.close)
+
+        self.output_edit = QtWidgets.QPlainTextEdit()
+        self.output_edit.setFont(QtGui.QFont('Fira Mono', 12))
+
+        self.source_edit = TextEdit()
+        self.source_edit.setFont(QtGui.QFont('Fira Mono', 12))
+        self.source_edit.submitted.connect(self._client.evaluate)
+
+        self.splitter = QtWidgets.QSplitter(Qt.Vertical)
+        self.splitter.addWidget(self.output_edit)
+        self.splitter.addWidget(self.source_edit)
+        self.splitter.setStretchFactor(0, 2)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.splitter)
+
+        self.addAction(self.action_quit)
+        self.setLayout(self.layout)
+
+        self.source_edit.setFocus()
+
+    def _output(self):
+        for line in self._client.output():
+            self.output_edit.insertPlainText(line)
+
+class TextEdit(QtWidgets.QPlainTextEdit):
+    submitted = QtCore.Signal(str)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Return:
+            self.submitted.emit(self.toPlainText())
+            self.clear()
+        else:
+            super().keyPressEvent(event)
+
+def start_thread(func, *args, **kwargs):
+    t = threading.Thread(target=func, args=args, kwargs=kwargs)
+    t.daemon = True
+    t.start()
+    return t
 
 def main():
     app = QtWidgets.QApplication()
 
-    action_quit = QtWidgets.QAction()
-    action_quit.setShortcut(QtGui.QKeySequence.Quit)
-    action_quit.triggered.connect(app.quit)
-
-    action_exec = QtWidgets.QAction()
-    action_exec.setShortcut(QtGui.QKeySequence(Qt.CTRL, Qt.Key_Enter))
-    action_exec.triggered.connect(execute)
-
-    output_edit = QtWidgets.QPlainTextEdit()
-    source_edit = QtWidgets.QPlainTextEdit()
-    source_edit.addAction(action_exec)
-
-    splitter = QtWidgets.QSplitter(Qt.Vertical)
-    splitter.addWidget(output_edit)
-    splitter.addWidget(source_edit)
-    splitter.setStretchFactor(0, 2)
-
-    layout = QtWidgets.QVBoxLayout()
-    layout.addWidget(splitter)
-
-    win = QtWidgets.QWidget()
-    win.addAction(action_quit)
-    win.setLayout(layout)
-
-    source_edit.setFocus()
+    win = Window(URL)
     win.show()
 
     app.exec_()
