9b3d67400e0d3efe11eeace3dc549fa7e08cd06b
dhagrow <cymrow@gmail.com>
2022-06-15 23:19:50 -0400

    restored symlinked setup dirs. alternative was not collecting all files

--- setup/lib/setup.py
+++ setup/lib/setup.py
@@ -1,9 +1,7 @@
 import os
-import sys
 from setuptools import setup, find_packages
 
-os.chdir(os.path.join(os.path.dirname(__file__), '../..'))
-sys.path.insert(0, '.')
+os.chdir(os.path.dirname(__file__))
 
 import telepythy
 
