ab3fac387f2a40be532d1395c960603630bb835b
dhagrow <cymrow@gmail.com>
2022-08-10 16:58:24 -0400

    scripts: added scripts and config for nuitka+nsis installer builds

---
+++ scripts/nuitka-build.bat
@@ -0,0 +1,16 @@
+@echo off
+
+@echo from telepythy.gui.__main__ import main; main() > telepythy.py
+
+python -OO -m nuitka^
+ --plugin-enable=pyside6^
+ --include-module=pygments.lexers.python^
+ --include-module=pygments.styles^
+ --include-package-data=telepythy^
+ --disable-console^
+ --windows-icon-from-ico=res/snake.ico^
+ --standalone^
+ --output-dir=build/^
+ .\telepythy.py
+
+ del telepythy.py
