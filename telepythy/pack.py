70b4c1eed24dbd8da5bc4a41a9c997af92ceba31
dhagrow <cymrow@gmail.com>
2022-08-16 11:57:39 -0400

    pack: small fix and simplified

--- telepythy/pack.py
+++ telepythy/pack.py
@@ -6,17 +6,10 @@ import glob
 import zipfile
 import argparse
 
+from .lib import utils
 from .lib import logs
 
-BASE_PATH = os.path.join(os.path.dirname(__file__), '..')
-
-def get_path(*names):
-    return os.path.abspath(os.path.join(BASE_PATH, *names))
-
-MAIN = """\
-from telepythy.__main__ import run
-run()
-"""
+MAIN = 'from telepythy.__main__ import run; run()'
 
 log = logs.get('telepythy.pack')
 
@@ -25,7 +18,8 @@ def pack(dst_path=None):
     if getattr(sys, 'frozen', False):
         return
 
-    src_path = get_path('telepythy/**/*.py')
+    src_path = utils.get_path('telepythy/**/*.py')
+    dst_path  = dst_path or utils.get_path('telepythy/telepythy_service.pyz')
 
     with zipfile.ZipFile(dst_path, 'w', zipfile.ZIP_DEFLATED,
             allowZip64=False) as zip:
@@ -44,9 +38,8 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-o', '--output-path',
-        default=get_path('telepythy/telepythy_service.pyz'),
+        default=utils.get_path('telepythy/telepythy_service.pyz'),
         help='path for the service package (default: %(default)s)')
-
     parser.add_argument('-v', '--verbose', action='count', default=0,
         help='enable verbose output (-vv for more)')
 
